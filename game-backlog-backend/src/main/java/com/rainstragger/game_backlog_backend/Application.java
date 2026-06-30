45ab081dc1af90772c5f76d71d4e60ffa375a626
Guilherme Miranda de Carvalho <guimi.univ@gmail.com>
2026-06-30 08:28:31 -0300

    add: mainclass() Applicatin

---
+++ game-backlog-backend/src/main/java/com/rainstragger/game_backlog_backend/Application.java
@@ -0,0 +1,12 @@
+package com.rainstragger.game_backlog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBacklogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameBacklogBackendApplication.class, args);
+    }
+}
